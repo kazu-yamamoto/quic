0768cca59c011119ac51c3a56b1cd6ce0c7e336e
Kazu Yamamoto <kazu@iij.ad.jp>
2019-01-26 12:36:14 +0900

    encodeInt.

--- Network/QUIC/Transport.hs
+++ Network/QUIC/Transport.hs
@@ -1,9 +1,11 @@
 module Network.QUIC.Transport (
     module Network.QUIC.Transport.Context
   , module Network.QUIC.Transport.Decode
+  , module Network.QUIC.Transport.Encode
   , module Network.QUIC.Transport.Types
   ) where
 
 import Network.QUIC.Transport.Context
 import Network.QUIC.Transport.Decode
+import Network.QUIC.Transport.Encode
 import Network.QUIC.Transport.Types
