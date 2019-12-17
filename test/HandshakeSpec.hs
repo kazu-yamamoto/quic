d125b823c4afe9f6f2638a366ee7fc52a4ddba51
Kazu Yamamoto <kazu@iij.ad.jp>
2019-12-17 16:45:50 +0900

    adding test case for TLS resumption.

--- test/HandshakeSpec.hs
+++ test/HandshakeSpec.hs
@@ -1,7 +1,10 @@
 module HandshakeSpec where
 
+
 import Control.Concurrent.Async
+import qualified Control.Exception as E
 import Control.Monad
+import Data.IORef
 import Network.TLS
 import Test.Hspec
 
@@ -29,6 +32,23 @@ spec = do
                        scRequireRetry = True
                      }
             testHandshake cc sc FullHandshake
+        it "can handshake in the case of resumption" $ do
+            ref <- newIORef Nothing
+            smgr <- newSessionManager
+            let cc = defaultClientConfig {
+                       ccConfig = defaultConfig {
+                                    confSessionManager = sessionManager ref
+                                  }
+                     }
+                sc = defaultServerConfig {
+                       scConfig = defaultConfig {
+                                    confSessionManager = smgr
+                                  }
+                     }
+                makeCc = do
+                    msession <- readIORef ref
+                    return $ cc { ccResume = msession }
+            testHandshake2 makeCc sc (FullHandshake, PreSharedKey)
 
 testHandshake :: ClientConfig -> ServerConfig -> HandshakeMode13 -> IO ()
 testHandshake cc sc mode = void $ concurrently client server
@@ -47,3 +67,49 @@ testHandshake cc sc mode = void $ concurrently client server
         isConnectionOpen conn `shouldReturn` True
         getTLSMode conn `shouldReturn` mode
         close conn
+
+testHandshake2 :: IO ClientConfig -> ServerConfig -> (HandshakeMode13, HandshakeMode13) -> IO ()
+testHandshake2 makeCc sc (mode1, mode2) = void $ concurrently client server
+  where
+    sc' = sc {
+            scKey  = "test/serverkey.pem"
+          , scCert = "test/servercert.pem"
+          }
+    handler (E.SomeException e) = print e
+    runClient cc mode = E.handle handler $ withQUICClient cc $ \qc -> do
+        conn <- connect qc
+        isConnectionOpen conn `shouldReturn` True
+        getTLSMode conn `shouldReturn` mode
+        close conn
+    client = do
+        cc1 <- makeCc
+        runClient cc1 mode1
+        cc2 <- makeCc
+        runClient cc2 mode2
+    runServer qs = do
+        conn <- accept qs
+        isConnectionOpen conn `shouldReturn` True
+        close conn
+    server = E.handle handler $ withQUICServer sc' $ \qs -> do
+        runServer qs
+        runServer qs
+
+newSessionManager :: IO SessionManager
+newSessionManager = sessionManager <$> newIORef Nothing
+
+sessionManager :: IORef (Maybe (SessionID, SessionData)) -> SessionManager
+sessionManager ref = SessionManager {
+    sessionEstablish      = establish
+  , sessionResume         = resume
+  , sessionResumeOnlyOnce = resume
+  , sessionInvalidate     = \_ -> return ()
+  }
+  where
+    establish sid sdata = writeIORef ref $ Just (sid,sdata)
+    resume sid = do
+        mx <- readIORef ref
+        case mx of
+          Nothing -> return Nothing
+          Just (s,d)
+            | s == sid  -> return $ Just d
+            | otherwise -> return Nothing
