c5068b2eecc537b2ab02fd1d5b70e159b3016abf
Kazu Yamamoto <kazu@iij.ad.jp>
2020-01-15 13:15:15 +0900

    splitting modules.

--- Network/QUIC/Connection.hs
+++ Network/QUIC/Connection.hs
@@ -1,6 +1,7 @@
 module Network.QUIC.Connection (
     module Network.QUIC.Connection.Crypto
   , module Network.QUIC.Connection.Misc
+  , module Network.QUIC.Connection.PacketNumber
   , module Network.QUIC.Connection.State
   , module Network.QUIC.Connection.StreamTable
   , module Network.QUIC.Connection.Transmit
@@ -9,6 +10,7 @@ module Network.QUIC.Connection (
 
 import Network.QUIC.Connection.Crypto
 import Network.QUIC.Connection.Misc
+import Network.QUIC.Connection.PacketNumber
 import Network.QUIC.Connection.State
 import Network.QUIC.Connection.StreamTable
 import Network.QUIC.Connection.Transmit
--- Network/QUIC/Connection/Transmit.hs
+++ Network/QUIC/Connection/Transmit.hs
@@ -2,13 +2,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Network.QUIC.Connection.Transmit (
-    getPacketNumber
-  , getPNs
-  , addPNs
-  , clearPNs
-  , nullPNs
-  , fromPNs
-  , keepOutput
+    keepOutput
   , releaseOutput
   , releaseOutputRemoveAcks
   , getRetransmissions
@@ -18,70 +12,12 @@ module Network.QUIC.Connection.Transmit (
 import Data.Hourglass
 import Data.IORef
 import qualified Data.IntPSQ as PSQ
-import qualified Data.Set as Set
 import System.Hourglass
 
+import Network.QUIC.Connection.PacketNumber
 import Network.QUIC.Connection.Types
 import Network.QUIC.Imports
 import Network.QUIC.Types
-
-----------------------------------------------------------------
--- My packet numbers
-
-getPacketNumber :: Connection -> IO PacketNumber
-getPacketNumber Connection{..} = atomicModifyIORef' packetNumber inc
-  where
-    inc pn = (pn + 1, pn)
-
-----------------------------------------------------------------
--- Peer's packet numbers
-
-getPNs :: Connection -> EncryptionLevel -> IO PeerPacketNumbers
-getPNs Connection{..} lvl = get <$> readIORef peerPacketNumbers
-  where
-    get (PeerPacketNumbers pns)  = PeerPacketNumbers . Set.map (convert lvl) . Set.filter (range lvl) $ pns
-
-addPNs :: Connection -> EncryptionLevel -> PacketNumber -> IO ()
-addPNs Connection{..} lvl pn = atomicModifyIORef' peerPacketNumbers add
-  where
-    add (PeerPacketNumbers pns) = (PeerPacketNumbers (Set.insert (convert lvl pn) pns), ())
-
-clearPNs :: Connection -> EncryptionLevel -> IO ()
-clearPNs Connection{..} lvl = atomicModifyIORef' peerPacketNumbers clear
-  where
-    clear (PeerPacketNumbers pns) = (PeerPacketNumbers (Set.filter (not . range lvl) pns), ())
-
-updatePNs :: Connection -> EncryptionLevel -> PeerPacketNumbers -> IO ()
-updatePNs Connection{..} lvl (PeerPacketNumbers pns) = atomicModifyIORef' peerPacketNumbers update
-  where
-    pns' = Set.map (convert lvl) pns
-    update (PeerPacketNumbers pns0) = (PeerPacketNumbers (pns0 Set.\\ pns'), ())
-
-----------------------------------------------------------------
-
-initialMagic, handshakeMagic :: PacketNumber
-initialMagic   = -2000
-handshakeMagic = -1000
-
-convert :: EncryptionLevel -> PacketNumber -> PacketNumber
-convert InitialLevel   pn = initialMagic - pn
-convert RTT0Level      pn = pn
-convert HandshakeLevel pn = handshakeMagic - pn
-convert RTT1Level      pn = pn
-
-range :: EncryptionLevel -> PacketNumber -> Bool
-range InitialLevel   pn = pn <= initialMagic
-range RTT0Level      pn = 0 <= pn
-range HandshakeLevel pn = initialMagic < pn && pn <= handshakeMagic
-range RTT1Level      pn = 0 <= pn
-
-----------------------------------------------------------------
-
-nullPNs :: PeerPacketNumbers -> Bool
-nullPNs (PeerPacketNumbers pns) = Set.null pns
-
-fromPNs :: PeerPacketNumbers -> [PacketNumber]
-fromPNs (PeerPacketNumbers pns) = Set.toDescList pns
 
 ----------------------------------------------------------------
 
