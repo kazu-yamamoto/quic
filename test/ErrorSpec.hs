b3d223bbf8e2caf830bc8517100b33551d0bbb5b
Kazu Yamamoto <kazu@iij.ad.jp>
2020-11-12 10:50:47 +0900

    better wordings.

--- test/ErrorSpec.hs
+++ test/ErrorSpec.hs
@@ -39,18 +39,18 @@ runC cc body = timeout 2000000 $ runQUICClient cc body
 
 spec' :: ClientConfig -> SpecWith a
 spec' cc = do
-    describe "A QUIC server" $ do
-        it "sends PROTOCOL_VIOLATION if reserved bits are non-zero [Transport 17.2]" $ \_ -> do
+    describe "QUIC servers" $ do
+        it "MUST send PROTOCOL_VIOLATION if reserved bits are non-zero [Transport 17.2]" $ \_ -> do
             let cc0 = addHook cc $ setOnPlainCreated $ rrBits InitialLevel
             runC cc0 waitEstablished `shouldThrow` check ProtocolViolation
             let cc1 = addHook cc $ setOnPlainCreated $ rrBits HandshakeLevel
             runC cc1 waitEstablished `shouldThrow` check ProtocolViolation
             let cc2 = addHook cc $ setOnPlainCreated $ rrBits RTT1Level
             runC cc2 waitEstablished `shouldThrow` check ProtocolViolation
-        it "sends TRANSPORT_PARAMETER_ERROR if initial source connection is missing [Transport 7.3]" $ \_ -> do
+        it "MUST send TRANSPORT_PARAMETER_ERROR if initial source connection is missing [Transport 7.3]" $ \_ -> do
             let cc0 = addHook cc $ setOnTransportParametersCreated dropInitialSourceConnectionId
             runC cc0 waitEstablished `shouldThrow` check TransportParameterError
-        it "sends FRAME_ENCODING_ERROR if reserved bits are non-zero [Transport 12.4]" $ \_ -> do
+        it "MUST send FRAME_ENCODING_ERROR if reserved bits are non-zero [Transport 12.4]" $ \_ -> do
             let cc0 = addHook cc $ setOnPlainCreated $ unknownFrame RTT1Level
             runC cc0 waitEstablished `shouldThrow` check FrameEncodingError
 
