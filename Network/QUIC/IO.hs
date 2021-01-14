fd5c24ed8b8563821ab7664d055f060e867dd360
Kazu Yamamoto <kazu@iij.ad.jp>
2021-01-14 19:50:06 +0900

    removing ConnectionIsClosed from API.

--- Network/QUIC/IO.hs
+++ Network/QUIC/IO.hs
@@ -14,16 +14,12 @@ import Network.QUIC.Types
 -- | Creating a bidirectional stream.
 stream :: Connection -> IO Stream
 stream conn = do
-    openC <- isConnectionOpen conn
-    unless openC $ E.throwIO ConnectionIsClosed
     sid <- getMyNewStreamId conn
     addStream conn sid
 
 -- | Creating a unidirectional stream.
 unidirectionalStream :: Connection -> IO Stream
 unidirectionalStream conn = do
-    openC <- isConnectionOpen conn
-    unless openC $ E.throwIO ConnectionIsClosed
     sid <- getMyNewUniStreamId conn
     addStream conn sid
 
@@ -34,8 +30,6 @@ sendStream s dat = sendStreamMany s [dat]
 -- | Sending a list of data in the stream.
 sendStreamMany :: Stream -> [ByteString] -> IO ()
 sendStreamMany s dats = do
-    closed <- isClosed $ streamConnection s
-    when closed $ E.throwIO ConnectionIsClosed
     sclosed <- isTxStreamClosed s
     when sclosed $ E.throwIO StreamIsClosed
     let len = totalLen dats
@@ -54,8 +48,6 @@ sendStreamMany s dats = do
 -- | Sending FIN in the stream.
 shutdownStream :: Stream -> IO ()
 shutdownStream s = do
-    closed <- isClosed $ streamConnection s
-    when closed $ E.throwIO ConnectionIsClosed
     sclosed <- isTxStreamClosed s
     when sclosed $ E.throwIO StreamIsClosed
     setTxStreamClosed s
@@ -64,8 +56,6 @@ shutdownStream s = do
 -- | Closing a stream. FIN is sent if necessary.
 closeStream :: Stream -> IO ()
 closeStream s = do
-    closed <- isClosed $ streamConnection s
-    when closed $ E.throwIO ConnectionIsClosed
     sclosed <- isTxStreamClosed s
     unless sclosed $ do
         setTxStreamClosed s
@@ -81,18 +71,13 @@ closeStream s = do
 -- | Accepting a stream initiated by the peer.
 acceptStream :: Connection -> IO Stream
 acceptStream conn = do
-    openC <- isConnectionOpen conn
-    unless openC $ E.throwIO ConnectionIsClosed
     InpStream s <- takeInput conn
     return s
 
 -- | Receiving data in the stream. In the case where a FIN is received
 --   an empty bytestring is returned.
 recvStream :: Stream -> Int -> IO ByteString
-recvStream s n = do
-    closed <- isClosed $ streamConnection s
-    when closed $ E.throwIO ConnectionIsClosed
-    takeRecvStreamQwithSize s n
+recvStream s n = takeRecvStreamQwithSize s n
 
 isBlocked :: Stream -> Int -> IO (Maybe Blocked)
 isBlocked s n = do
