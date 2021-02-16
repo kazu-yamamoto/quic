a228451dde94f2f618b7cd57d3eda34c7b6e0150
Kazu Yamamoto <kazu@iij.ad.jp>
2021-02-16 15:03:47 +0900

    improving merge.

--- Network/QUIC/Stream/Skew.hs
+++ Network/QUIC/Stream/Skew.hs
@@ -2,19 +2,17 @@
 
 module Network.QUIC.Stream.Skew (
     Skew(..)
-  , empty
-  , singleton
   , insert
   , deleteMin
   , deleteMin2
-  , null
   , merge
   , minimum
   ) where
 
 import Control.Applicative hiding (empty)
 import Data.Maybe
-import Data.Sequence (Seq(..), viewl, viewr, ViewL(..), ViewR(..), singleton)
+import Data.Sequence (Seq(..), viewl, viewr, ViewL(..), ViewR(..), (><))
+import qualified Data.Sequence as Seq
 import Prelude hiding (minimum, maximum, null)
 
 ----------------------------------------------------------------
@@ -31,43 +29,31 @@ instance Frag a => Frag (Seq a) where
       EmptyR -> error "Seq is empty"
       _ :> x -> next x
 
+data F = F Int Int deriving Show
+
+instance Frag F where
+    start (F s _) = s
+    next  (F _ n) = n
+
 ----------------------------------------------------------------
 
 data Skew a = Leaf | Node (Skew a) (Seq a) (Skew a) deriving Show
 
-----------------------------------------------------------------
-
 empty :: Skew a
 empty = Leaf
-
-null :: Skew a -> Bool
-null Leaf         = True
-null (Node _ _ _) = False
 
 ----------------------------------------------------------------
 
 -- | Insertion. Worst-case: O(N), amortized: O(log N).
 insert :: Frag a => a -> Skew a -> Skew a
-insert x Leaf = Node Leaf (singleton x) Leaf
--- insert x t = merge (singleton x) t
-insert x t@(Node l s r)
-  | n1 == s2  = Node l (x :<| s) r
-  | n2 == s1  = Node l (s :|> x) r
-  | n1 <  s2  = Node Leaf (singleton x) t
-  | n2 <  s1  = Node r s (merge l (Node Leaf (singleton x) Leaf))
-  | otherwise = t
-  where
-    s1 = start x
-    n1 = next x
-    s2 = start s
-    n2 = next s
+insert x t = merge (Node Leaf (Seq.singleton x) Leaf) t
 
 ----------------------------------------------------------------
 
 -- | Finding the minimum element. Worst-case: O(1).
 minimum :: Skew a -> Maybe (Seq a)
 minimum Leaf         = Nothing
-minimum (Node _ x _) = Just x
+minimum (Node _ f _) = Just f
 
 ----------------------------------------------------------------
 
@@ -86,6 +72,18 @@ deleteMin2 h    = (, deleteMin h) <$> minimum h
 merge :: Frag a => Skew a -> Skew a -> Skew a
 merge t1 Leaf = t1
 merge Leaf t2 = t2
-merge t1@(Node l1 s1 r1) t2@(Node l2 s2 r2)
-  | start s2 < start s2 = Node r1 s1 (merge l1 t2)
-  | otherwise           = Node r2 s2 (merge l2 t1)
+merge t1@(Node l1 f1 r1) t2@(Node l2 f2 r2)
+  | e1 < s2   = Node r1 f1 (merge l1 t2)
+  | e2 < s1   = Node r2 f2 (merge l2 t1)
+  | otherwise = Node (merge l1 l2) f12 (merge r1 r2)
+  where
+    s1 = start f1
+    e1 = next  f1
+    s2 = start f2
+    e2 = next  f2
+    f12 | e1 == s2             = f1 >< f2
+        | s1 == e2             = f2 >< f1
+        | s1 <= s2 && e2 <= e1 = f1
+        | s2 <= s1 && e1 <= e2 = f2
+        | s1 <= s2             = f1 >< (undefined e1 f2)
+        | otherwise            = f2 >< (undefined e2 f1)
