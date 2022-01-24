d4b98ddcd7cd4414d564ea01b54ff421ae430cec
Kazu Yamamoto <kazu@iij.ad.jp>
2022-01-24 13:02:48 +0900

    adding test vector for version 2

--- test/TLSSpec.hs
+++ test/TLSSpec.hs
@@ -93,3 +93,77 @@ spec = do
             sample `shouldBe` Sample (dec16 "d1b1c98dd7689fb8ec11d242b123dc9b")
             let Mask mask = protectionMask defaultCipher chp sample
             BS.take 5 mask `shouldBe` dec16 "437b9aec36"
+
+    describe "test vector for version 2" $ do
+        let ver = Version2
+        it "describes the examples of Keys" $ do
+            ----------------------------------------------------------------
+            -- shared keys
+            let dcID = makeCID (dec16s "8394c8f03e515708")
+            let client_initial_secret@(ClientTrafficSecret cis) = clientInitialSecret ver dcID
+            client_initial_secret `shouldBe` ClientTrafficSecret (dec16 "9fe72e1452e91f551b770005054034e47575d4a0fb4c27b7c6cb303a338423ae")
+            let ckey = aeadKey ver defaultCipher (Secret cis)
+            ckey `shouldBe` Key (dec16 "95df2be2e8d549c82e996fc9339f4563")
+            let civ = initialVector ver defaultCipher (Secret cis)
+            civ `shouldBe` IV (dec16 "ea5e3c95f933db14b7020ad8")
+            let chp = headerProtectionKey ver defaultCipher (Secret cis)
+            chp `shouldBe` Key (dec16 "091efb735702447d07908f6501845794")
+            let server_initial_secret@(ServerTrafficSecret sis) = serverInitialSecret ver dcID
+            server_initial_secret `shouldBe` ServerTrafficSecret (dec16 "3c9bf6a9c1c8c71819876967bd8b979efd98ec665edf27f22c06e9845ba0ae2f")
+            let skey = aeadKey ver defaultCipher (Secret sis)
+            skey `shouldBe` Key (dec16 "15d5b4d9a2b8916aa39b1bfe574d2aad")
+            let siv = initialVector ver defaultCipher (Secret sis)
+            siv `shouldBe` IV (dec16 "a85e7ac31cd275cbb095c626")
+            let shp = headerProtectionKey ver defaultCipher (Secret sis)
+            shp `shouldBe` Key (dec16 "b13861cfadbb9d11ff942dd80c8fc33b")
+
+        it "describes the examples of Client Initial" $ do
+            let dcID = makeCID (dec16s "8394c8f03e515708")
+                ClientTrafficSecret cis = clientInitialSecret ver dcID
+                ckey = aeadKey ver defaultCipher (Secret cis)
+                civ = initialVector ver defaultCipher (Secret cis)
+                chp = headerProtectionKey ver defaultCipher (Secret cis)
+            ----------------------------------------------------------------
+            -- payload encryption
+            let clientCRYPTOframe = dec16 $ BS.concat [
+                    "060040f1010000ed0303ebf8fa56f12939b9584a3896472ec40bb863cfd3e868"
+                  , "04fe3a47f06a2b69484c00000413011302010000c000000010000e00000b6578"
+                  , "616d706c652e636f6dff01000100000a00080006001d00170018001000070005"
+                  , "04616c706e000500050100000000003300260024001d00209370b2c9caa47fba"
+                  , "baf4559fedba753de171fa71f50f1ce15d43e994ec74d748002b000302030400"
+                  , "0d0010000e0403050306030203080408050806002d00020101001c0002400100"
+                  , "3900320408ffffffffffffffff05048000ffff07048000ffff08011001048000"
+                  , "75300901100f088394c8f03e51570806048000ffff"
+                  ]
+            let clientPacketHeader = dec16 "d3709a50c4088394c8f03e5157080000449e00000002"
+            -- d3 709a50c4 08 8394c8f03e515708 00 00 449e 00000002
+            -- d3 (11010011)    -- flags
+            -- 709a50c4         -- version 2
+            -- 08               -- dcid len
+            -- 8394c8f03e515708 -- dcid
+            -- 00               -- scid len
+            -- 00               -- token length
+            -- 449e             -- length: decodeInt (dec16 "449e")
+                                -- 1182 = 4 + 1162 + 16 (fixme)
+            -- 00000002         -- encoded packet number
+
+            let bodyLen = fromIntegral $ decodeInt (dec16 "449e")
+            let padLen = bodyLen
+                       - 4  -- packet number length
+                       - 16 -- GCM encrypt expansion
+                       - BS.length clientCRYPTOframe
+                clientCRYPTOframePadded = clientCRYPTOframe `BS.append` BS.pack (replicate padLen 0)
+            let plaintext = clientCRYPTOframePadded
+            let nonce = makeNonce civ $ dec16 "00000002"
+            let add = AssDat clientPacketHeader
+            let Just (hdr,bdy) = niteEncrypt' defaultCipher ckey nonce plaintext add
+                ciphertext = hdr `BS.append` bdy
+            let Just plaintext' = niteDecrypt' defaultCipher ckey nonce ciphertext add
+            plaintext' `shouldBe` plaintext
+
+            ----------------------------------------------------------------
+            -- header protection
+            let sample = Sample (BS.take 16 ciphertext)
+            sample `shouldBe` Sample (dec16 "23b8e610589c83c92d0e97eb7a6e5003")
+            let Mask mask = protectionMask defaultCipher chp sample
+            BS.take 5 mask `shouldBe` dec16 "8e4391d84a"
