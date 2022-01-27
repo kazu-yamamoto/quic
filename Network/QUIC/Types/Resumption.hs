c7702ad3bfc743cff0d7afea625591477085ef9b
Kazu Yamamoto <kazu@iij.ad.jp>
2022-01-27 17:27:35 +0900

    Using the negotiated version when using ticket and token.

--- Network/QUIC/Types/Resumption.hs
+++ Network/QUIC/Types/Resumption.hs
@@ -2,24 +2,27 @@
 
 module Network.QUIC.Types.Resumption where
 
-import Network.TLS
+import Network.TLS hiding (Version)
 import Network.TLS.QUIC
 
 import Network.QUIC.Imports
 import Network.QUIC.Types.Frame
+import Network.QUIC.Types.Packet
 
 type SessionEstablish = SessionID -> SessionData -> IO ()
 
 -- | Information about resumption
 data ResumptionInfo = ResumptionInfo {
-    resumptionSession :: Maybe (SessionID, SessionData)
+    resumptionVersion :: Version
+  , resumptionSession :: Maybe (SessionID, SessionData)
   , resumptionToken   :: Token
   , resumptionRetry   :: Bool
   } deriving (Eq, Show)
 
 defaultResumptionInfo :: ResumptionInfo
 defaultResumptionInfo = ResumptionInfo {
-    resumptionSession = Nothing
+    resumptionVersion = chosenVersion defaultVersionInfo
+  , resumptionSession = Nothing
   , resumptionToken   = emptyToken
   , resumptionRetry   = False
   }
@@ -41,4 +44,3 @@ get0RTTCipher :: ResumptionInfo -> Maybe CipherID
 get0RTTCipher ri = case resumptionSession ri of
   Nothing      -> Nothing
   Just (_, sd) -> Just $ sessionCipher sd
-
