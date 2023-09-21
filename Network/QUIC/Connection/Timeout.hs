5cf468e3a7fa6aac2a654a816fe99b1d5bdc1f68
Kazu Yamamoto <kazu@iij.ad.jp>
2023-09-21 11:29:58 +0900

    using System.Timeout.timeout
    ensuring that SystemTimeoutManager is not blocked.

--- Network/QUIC/Connection/Timeout.hs
+++ Network/QUIC/Connection/Timeout.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-
 module Network.QUIC.Connection.Timeout (
     timeout
   , fire
@@ -8,37 +5,18 @@ module Network.QUIC.Connection.Timeout (
   , delay
   ) where
 
-import Data.Typeable
 import Network.QUIC.Event
 import UnliftIO.Concurrent
 import qualified UnliftIO.Exception as E
+import qualified System.Timeout as ST
 
 import Network.QUIC.Connection.Types
 import Network.QUIC.Connector
 import Network.QUIC.Imports
 import Network.QUIC.Types
 
-data TimeoutException = TimeoutException String deriving (Show, Typeable)
-
-instance E.Exception TimeoutException where
-  fromException = E.asyncExceptionFromException
-  toException = E.asyncExceptionToException
-
--- action MUST not include FFI.
--- If so, the thread of getSystemTimerManager would be blocked.
 timeout :: Microseconds -> String -> IO a -> IO (Maybe a)
-timeout (Microseconds ms) dmsg action = do
-    tid <- myThreadId
-    timmgr <- getSystemTimerManager
-#if defined(mingw32_HOST_OS)
-    let killMe = void $ forkIO $ E.throwTo tid $ TimeoutException dmsg
-#else
-    let killMe = E.throwTo tid $ TimeoutException dmsg
-#endif
-        setup = registerTimeout timmgr ms killMe
-        cleanup key = unregisterTimeout timmgr key
-    E.handleSyncOrAsync (\(TimeoutException _) -> return Nothing) $
-        E.bracket setup cleanup $ \_ -> Just <$> action
+timeout (Microseconds ms) _ action = ST.timeout ms action
 
 fire :: Connection -> Microseconds -> TimeoutCallback -> IO ()
 fire conn (Microseconds microseconds) action = do
