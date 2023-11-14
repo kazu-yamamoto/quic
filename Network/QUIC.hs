8ffad2c04eb23a0ca58221cc1a24f4a268a50d66
Kazu Yamamoto <kazu@iij.ad.jp>
2023-11-14 15:12:50 +0900

    fourmolu

--- Network/QUIC.hs
+++ Network/QUIC.hs
@@ -4,47 +4,75 @@
 --
 -- The -threaded option must be specified to GHC to use this library.
 module Network.QUIC (
-  -- * Connection
-    Connection
-  , abortConnection
-  -- * Stream
-  , Stream
-  , StreamId
-  , streamId
-  -- ** Category
-  , isClientInitiatedBidirectional
-  , isServerInitiatedBidirectional
-  , isClientInitiatedUnidirectional
-  , isServerInitiatedUnidirectional
-  -- ** Opening
-  , stream
-  , unidirectionalStream
-  , acceptStream
-  -- ** Closing
-  , closeStream
-  , shutdownStream
-  , resetStream
-  , stopStream
-  -- * IO
-  , recvStream
-  , sendStream
-  , sendStreamMany
-  -- * Information
-  , ConnectionInfo(..)
-  , getConnectionInfo
-  -- * Statistics
-  , ConnectionStats(..)
-  , getConnectionStats
-  -- * Synchronization
-  , wait0RTTReady
-  , wait1RTTReady
-  , waitEstablished
-  -- * Exceptions and Errors
-  , QUICException(..)
-  , TransportError(.., NoError, InternalError, ConnectionRefused, FlowControlError, StreamLimitError, StreamStateError, FinalSizeError, FrameEncodingError, TransportParameterError, ConnectionIdLimitError, ProtocolViolation, InvalidToken, ApplicationError, CryptoBufferExceeded, KeyUpdateError, AeadLimitReached, NoViablePath)
-  , cryptoError
-  , ApplicationProtocolError(..)
-  ) where
+    -- * Connection
+    Connection,
+    abortConnection,
+
+    -- * Stream
+    Stream,
+    StreamId,
+    streamId,
+
+    -- ** Category
+    isClientInitiatedBidirectional,
+    isServerInitiatedBidirectional,
+    isClientInitiatedUnidirectional,
+    isServerInitiatedUnidirectional,
+
+    -- ** Opening
+    stream,
+    unidirectionalStream,
+    acceptStream,
+
+    -- ** Closing
+    closeStream,
+    shutdownStream,
+    resetStream,
+    stopStream,
+
+    -- * IO
+    recvStream,
+    sendStream,
+    sendStreamMany,
+
+    -- * Information
+    ConnectionInfo (..),
+    getConnectionInfo,
+
+    -- * Statistics
+    ConnectionStats (..),
+    getConnectionStats,
+
+    -- * Synchronization
+    wait0RTTReady,
+    wait1RTTReady,
+    waitEstablished,
+
+    -- * Exceptions and Errors
+    QUICException (..),
+    TransportError (
+        ..,
+        NoError,
+        InternalError,
+        ConnectionRefused,
+        FlowControlError,
+        StreamLimitError,
+        StreamStateError,
+        FinalSizeError,
+        FrameEncodingError,
+        TransportParameterError,
+        ConnectionIdLimitError,
+        ProtocolViolation,
+        InvalidToken,
+        ApplicationError,
+        CryptoBufferExceeded,
+        KeyUpdateError,
+        AeadLimitReached,
+        NoViablePath
+    ),
+    cryptoError,
+    ApplicationProtocolError (..),
+) where
 
 import Network.QUIC.Connection
 import Network.QUIC.IO
