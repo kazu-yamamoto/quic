f81d3190f9f4c66d10b8c60d15e60c8c58cd1b78
Kazu Yamamoto <kazu@iij.ad.jp>
2024-06-25 15:05:33 +0900

    stop using old tls

--- Network/QUIC/Handshake.hs
+++ Network/QUIC/Handshake.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -25,18 +24,18 @@ import Network.QUIC.Types
 ----------------------------------------------------------------
 
 newtype HndState = HndState
-    { hsRecvCnt :: Int  -- number of 'recv' calls since last 'send'
+    { hsRecvCnt :: Int -- number of 'recv' calls since last 'send'
     }
 
 newHndStateRef :: IO (IORef HndState)
-newHndStateRef = newIORef HndState { hsRecvCnt = 0 }
+newHndStateRef = newIORef HndState{hsRecvCnt = 0}
 
 sendCompleted :: IORef HndState -> IO ()
-sendCompleted hsr = atomicModifyIORef'' hsr $ \hs -> hs { hsRecvCnt = 0 }
+sendCompleted hsr = atomicModifyIORef'' hsr $ \hs -> hs{hsRecvCnt = 0}
 
 recvCompleted :: IORef HndState -> IO Int
 recvCompleted hsr = atomicModifyIORef' hsr $ \hs ->
-    let cnt = hsRecvCnt hs in (hs { hsRecvCnt = cnt + 1 }, cnt)
+    let cnt = hsRecvCnt hs in (hs{hsRecvCnt = cnt + 1}, cnt)
 
 rxLevelChanged :: IORef HndState -> IO ()
 rxLevelChanged = sendCompleted
@@ -49,30 +48,43 @@ sendCryptoData = putOutput
 recvCryptoData :: Connection -> IO Crypto
 recvCryptoData = takeCrypto
 
-recvTLS :: Connection -> IORef HndState -> CryptLevel -> IO (Either TLS.TLSError ByteString)
+recvTLS
+    :: Connection
+    -> IORef HndState
+    -> CryptLevel
+    -> IO (Either TLS.TLSError ByteString)
 recvTLS conn hsr level =
     case level of
-            CryptInitial           -> go InitialLevel
-            CryptMainSecret        -> failure "QUIC does not receive data < TLS 1.3"
-            CryptEarlySecret       -> failure "QUIC does not send early data with TLS library"
-            CryptHandshakeSecret   -> go HandshakeLevel
-            CryptApplicationSecret -> go RTT1Level
+        CryptInitial -> go InitialLevel
+        CryptMainSecret -> failure "QUIC does not receive data < TLS 1.3"
+        CryptEarlySecret -> failure "QUIC does not send early data with TLS library"
+        CryptHandshakeSecret -> go HandshakeLevel
+        CryptApplicationSecret -> go RTT1Level
   where
     failure = return . Left . internalError
 
     go expected = do
         InpHandshake actual bs <- recvCryptoData conn
-        if bs == "" then
-            return $ Left TLS.Error_EOF
-          else if actual /= expected then
-            failure $ "encryption level mismatch: expected " ++ show expected ++ " but got " ++ show actual
-          else do
-            when (isClient conn) $ do
-                n <- recvCompleted hsr
-                -- Sending ACKs for three times rule
-                when ((n `mod` 3) == 1) $
-                    sendCryptoData conn $ OutControl HandshakeLevel [] $ return ()
-            return $ Right bs
+        if bs == ""
+            then
+                return $ Left TLS.Error_EOF
+            else
+                if actual /= expected
+                    then
+                        failure $
+                            "encryption level mismatch: expected "
+                                ++ show expected
+                                ++ " but got "
+                                ++ show actual
+                    else do
+                        when (isClient conn) $ do
+                            n <- recvCompleted hsr
+                            -- Sending ACKs for three times rule
+                            when ((n `mod` 3) == 1) $
+                                sendCryptoData conn $
+                                    OutControl HandshakeLevel [] $
+                                        return ()
+                        return $ Right bs
 
 sendTLS :: Connection -> IORef HndState -> [(CryptLevel, ByteString)] -> IO ()
 sendTLS conn hsr x = do
@@ -86,11 +98,7 @@ sendTLS conn hsr x = do
     convertLevel (CryptApplicationSecret, bs) = return (RTT1Level, bs)
 
 internalError :: String -> TLS.TLSError
-#if MIN_VERSION_tls(1,9,0)
-internalError msg     = TLS.Error_Protocol msg TLS.InternalError
-#else
-internalError msg     = TLS.Error_Protocol (msg, True, TLS.InternalError)
-#endif
+internalError msg = TLS.Error_Protocol msg TLS.InternalError
 
 ----------------------------------------------------------------
 
@@ -99,16 +107,20 @@ handshakeClient conf conn myAuthCIDs = do
     qlogParamsSet conn (ccParameters conf, "local") -- fixme
     handshakeClient' conf conn myAuthCIDs <$> getVersion conn <*> newHndStateRef
 
-handshakeClient' :: ClientConfig -> Connection -> AuthCIDs -> Version -> IORef HndState -> IO ()
+handshakeClient'
+    :: ClientConfig -> Connection -> AuthCIDs -> Version -> IORef HndState -> IO ()
 handshakeClient' conf conn myAuthCIDs ver hsr = handshaker
   where
-    handshaker = clientHandshaker qc conf ver myAuthCIDs setter use0RTT `E.catch` sendCCTLSError
-    qc = QUICCallbacks { quicSend = sendTLS conn hsr
-                       , quicRecv = recvTLS conn hsr
-                       , quicInstallKeys = installKeysClient
-                       , quicNotifyExtensions = setPeerParams conn
-                       , quicDone = done
-                       }
+    handshaker =
+        clientHandshaker qc conf ver myAuthCIDs setter use0RTT `E.catch` sendCCTLSError
+    qc =
+        QUICCallbacks
+            { quicSend = sendTLS conn hsr
+            , quicRecv = recvTLS conn hsr
+            , quicInstallKeys = installKeysClient
+            , quicNotifyExtensions = setPeerParams conn
+            , quicDone = done
+            }
     setter = setResumptionSession conn
     installKeysClient _ctx (InstallEarlyKeys Nothing) = return ()
     installKeysClient _ctx (InstallEarlyKeys (Just (EarlySecretInfo cphr cts))) = do
@@ -134,10 +146,10 @@ handshakeClient' conf conn myAuthCIDs ver hsr = handshaker
         -- Validating Chosen Version
         mPeerVerInfo <- versionInformation <$> getPeerParameters conn
         case mPeerVerInfo of
-          Nothing -> return ()
-          Just peerVerInfo -> do
-              hdrVer <- getVersion conn
-              when (hdrVer /= chosenVersion peerVerInfo) sendCCVNError
+            Nothing -> return ()
+            Just peerVerInfo -> do
+                hdrVer <- getVersion conn
+                when (hdrVer /= chosenVersion peerVerInfo) sendCCVNError
         info <- getConnectionInfo conn
         connDebugLog conn $ bhow info
     use0RTT = ccUse0RTT conf
@@ -146,22 +158,31 @@ handshakeClient' conf conn myAuthCIDs ver hsr = handshaker
 
 handshakeServer :: ServerConfig -> Connection -> AuthCIDs -> IO (IO ())
 handshakeServer conf conn myAuthCIDs =
-    handshakeServer' conf conn <$> getVersion conn
-                               <*> newHndStateRef
-                               <*> newIORef params
+    handshakeServer' conf conn
+        <$> getVersion conn
+        <*> newHndStateRef
+        <*> newIORef params
   where
     params = setCIDsToParameters myAuthCIDs $ scParameters conf
 
-handshakeServer' :: ServerConfig -> Connection -> Version -> IORef HndState -> IORef Parameters -> IO ()
+handshakeServer'
+    :: ServerConfig
+    -> Connection
+    -> Version
+    -> IORef HndState
+    -> IORef Parameters
+    -> IO ()
 handshakeServer' conf conn ver hsRef paramRef = handshaker
   where
     handshaker = serverHandshaker qc conf ver getParams `E.catch` sendCCTLSError
-    qc = QUICCallbacks { quicSend = sendTLS conn hsRef
-                       , quicRecv = recvTLS conn hsRef
-                       , quicInstallKeys = installKeysServer
-                       , quicNotifyExtensions = setPeerParams conn
-                       , quicDone = done
-                       }
+    qc =
+        QUICCallbacks
+            { quicSend = sendTLS conn hsRef
+            , quicRecv = recvTLS conn hsRef
+            , quicInstallKeys = installKeysServer
+            , quicNotifyExtensions = setPeerParams conn
+            , quicDone = done
+            }
     installKeysServer _ctx (InstallEarlyKeys Nothing) = return ()
     installKeysServer _ctx (InstallEarlyKeys (Just (EarlySecretInfo cphr cts))) = do
         setCipher conn RTT0Level cphr
@@ -176,8 +197,8 @@ handshakeServer' conf conn ver hsRef paramRef = handshaker
     installKeysServer ctx (InstallApplicationKeys appSecInf@(ApplicationSecretInfo tss)) = do
         storeNegotiated conn ctx appSecInf
         initializeCoder1RTT conn tss
-        -- will switch to RTT1Level after client Finished
-        -- is received and verified
+    -- will switch to RTT1Level after client Finished
+    -- is received and verified
     done ctx = do
         setEncryptionLevel conn RTT1Level
         TLS.getClientCertificateChain ctx >>= setCertificateChain conn
@@ -194,14 +215,14 @@ handshakeServer' conf conn ver hsRef paramRef = handshaker
         setConnection1RTTReady conn
         setConnectionEstablished conn
         getConnectionInfo conn >>= onConnectionEstablished (connHooks conn)
---        sendFrames conn RTT1Level [HandshakeDone]
+        --        sendFrames conn RTT1Level [HandshakeDone]
         --
         info <- getConnectionInfo conn
         connDebugLog conn $ bhow info
     getParams = do
         params <- readIORef paramRef
         verInfo <- getVersionInfo conn
-        return params { versionInformation = Just verInfo }
+        return params{versionInformation = Just verInfo}
 
 ----------------------------------------------------------------
 
@@ -209,19 +230,21 @@ setPeerParams :: Connection -> TLS.Context -> [ExtensionRaw] -> IO ()
 setPeerParams conn _ctx peerExts = do
     tpId <- extensionIDForTtransportParameter <$> getVersion conn
     case getTP tpId peerExts of
-      Nothing                  -> sendCCTLSAlert TLS.MissingExtension "QUIC transport parameters are mssing"
-      Just (ExtensionRaw _ bs) -> setPP bs
+        Nothing ->
+            sendCCTLSAlert TLS.MissingExtension "QUIC transport parameters are mssing"
+        Just (ExtensionRaw _ bs) -> setPP bs
   where
     getTP n = find (\(ExtensionRaw extid _) -> extid == n)
     setPP bs = case decodeParameters bs of
-      Nothing     -> sendCCParamError
-      Just params -> do
-          checkAuthCIDs params
-          checkInvalid params
-          setParams params
-          qlogParamsSet conn (params,"remote")
-          when (isServer conn) $
-              serverVersionNegotiation $ versionInformation params
+        Nothing -> sendCCParamError
+        Just params -> do
+            checkAuthCIDs params
+            checkInvalid params
+            setParams params
+            qlogParamsSet conn (params, "remote")
+            when (isServer conn) $
+                serverVersionNegotiation $
+                    versionInformation params
 
     checkAuthCIDs params = do
         peerAuthCIDs <- getPeerAuthCIDs conn
@@ -231,20 +254,20 @@ setPeerParams conn _ctx peerExts = do
             ensure (retrySourceConnectionId params) $ retrySrcCID peerAuthCIDs
     ensure _ Nothing = return ()
     ensure v0 v1
-      | v0 == v1  = return ()
-      | otherwise = sendCCParamError
+        | v0 == v1 = return ()
+        | otherwise = sendCCParamError
     checkInvalid params = do
         when (maxUdpPayloadSize params < 1200) sendCCParamError
         when (ackDelayExponent params > 20) sendCCParamError
-        when (maxAckDelay params >= 2^(14 :: Int)) sendCCParamError
+        when (maxAckDelay params >= 2 ^ (14 :: Int)) sendCCParamError
         when (isServer conn) $ do
             when (isJust $ originalDestinationConnectionId params) sendCCParamError
             when (isJust $ preferredAddress params) sendCCParamError
             when (isJust $ retrySourceConnectionId params) sendCCParamError
             when (isJust $ statelessResetToken params) sendCCParamError
         let vi = case versionInformation params of
-              Nothing  -> VersionInfo Version1 [Version1]
-              Just vi0 -> vi0
+                Nothing -> VersionInfo Version1 [Version1]
+                Just vi0 -> vi0
         when (vi == brokenVersionInfo) sendCCParamError
         when (Negotiation `elem` otherVersions vi) sendCCParamError
         -- Always False for servers
@@ -252,13 +275,12 @@ setPeerParams conn _ctx peerExts = do
         when isICVN $ do
             -- Validating Other Version fields.
             verInfo <- getVersionInfo conn
-            let myVer  = chosenVersion verInfo
+            let myVer = chosenVersion verInfo
                 myVers = filter (not . isGreasingVersion) $ otherVersions verInfo
                 peerVers = otherVersions vi
             case myVers `intersect` peerVers of
-              ver:_ | ver == myVer -> return ()
-              _                    -> sendCCVNError
-
+                ver : _ | ver == myVer -> return ()
+                _ -> sendCCVNError
 
     setParams params = do
         setPeerParameters conn params
@@ -277,12 +299,12 @@ setPeerParams conn _ctx peerExts = do
             peerVers = otherVersions peerVerInfo
         -- Server's preference should be preferred.
         case myVers `intersect` peerVers of
-          vers@(serverVer:_)
-            | clientVer /= serverVer -> do
-                setVersionInfo conn $ VersionInfo serverVer vers
-                dcid <- getClientDstCID conn
-                initializeCoder conn InitialLevel $ initialSecrets serverVer dcid
-          _ -> return ()
+            vers@(serverVer : _)
+                | clientVer /= serverVer -> do
+                    setVersionInfo conn $ VersionInfo serverVer vers
+                    dcid <- getClientDstCID conn
+                    initializeCoder conn InitialLevel $ initialSecrets serverVer dcid
+            _ -> return ()
 
 storeNegotiated :: Connection -> TLS.Context -> ApplicationSecretInfo -> IO ()
 storeNegotiated conn ctx appSecInf = do
@@ -312,16 +334,10 @@ sendCCTLSAlert :: TLS.AlertDescription -> ReasonPhrase -> IO ()
 sendCCTLSAlert a msg = closeConnection (cryptoError a) msg
 
 getErrorCause :: TLS.TLSException -> TLS.TLSError
-getErrorCause (TLS.Terminated _ _ e)   = e
-getErrorCause (TLS.HandshakeFailed e)  = e
-#if MIN_VERSION_tls(1,8,0)
-getErrorCause (TLS.PostHandshake e)    = e
+getErrorCause (TLS.Terminated _ _ e) = e
+getErrorCause (TLS.HandshakeFailed e) = e
+getErrorCause (TLS.PostHandshake e) = e
 getErrorCause (TLS.Uncontextualized e) = e
-#endif
 getErrorCause e =
     let msg = "unexpected TLS exception: " ++ show e
-#if MIN_VERSION_tls(1,9,0)
      in TLS.Error_Protocol msg TLS.InternalError
-#else
-     in TLS.Error_Protocol (msg, True, TLS.InternalError)
-#endif
